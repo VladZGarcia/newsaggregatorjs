2d14fe814d6fb362b6bdb3878eb07b2506ea62c9
VladZGarcia <113976475+VladZGarcia@users.noreply.github.com>
2024-03-14 11:24:06 +0100

    article giving 404
    article page not ready yet

--- app/Article.js
+++ app/Article.js
@@ -3,7 +3,7 @@ import ReadMoreButton from './ReadMoreButton';
 
 function Article({ article }) {
   return (
-    // <a href={article.url} className="block">
+     <a href={article.url} className="block">
       <article className="bg-slate-100 dark:bg-slate-800 flex flex-col rounded-lg shadow-sm hover:scale-105 hover:shadow-lg hover:bg-slate-200 transition-all duration-200 ease-out">
         {article.image && (
           <img
@@ -31,7 +31,7 @@ function Article({ article }) {
           {/* <button className="bg-orange-400 h-10 rounded-b-lg dark:text-gray-900 hover:bg-orange-500">Leer más</button> */}
         </div>
       </article>
-    // </a>
+     </a>
   );
 }
 
