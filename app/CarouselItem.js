4c7dd1c5cd0fcaf18df1d055d2ca1359ddfa1fe8
VladZGarcia <113976475+VladZGarcia@users.noreply.github.com>
2024-04-16 14:51:37 +0200

    Hamburger menu added
    Simple hamburger menu

--- app/CarouselItem.js
+++ app/CarouselItem.js
@@ -11,7 +11,7 @@ function CarouselItem({ article }) {
   return (
     <a href={article.url} className="embla__container h-auto p-1 gap-10">
       <article className="relative bg-slate-100 dark:bg-slate-800 flex flex-col rounded-lg shadow-lg hover:scale-105 hover:shadow-xl hover:bg-slate-200 transition-all duration-200 ease-out embla__slide items-center justify-center h-auto ">
-        <h2 className="absolute top-0 left-0 right-0 text-center font-bold text-white bg-black bg-opacity-50 px-4 py-2 rounded-t-lg font-serif">
+        <h2 className="absolute top-0 left-0 right-0 text-center font-bold text-white bg-black bg-opacity-70 px-4 py-2 rounded-t-lg font-serif">
           {article.title}
         </h2>
         <div className="flex-1 flex flex-col justify-end w-full">
